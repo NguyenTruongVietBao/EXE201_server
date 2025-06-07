a2024d292c515bf413adda7d33a2ed0e48a75d4d
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2025-06-07 19:25:32 +0700

    fix init swagger

--- index.js
+++ index.js
@@ -6,32 +6,32 @@ const authRoutes = require('./routes/auth.route');
 const userRoutes = require('./routes/user.route');
 const interestRoutes = require('./routes/interest.route');
 const cors = require('cors');
-const {
-  swaggerUi,
-  swaggerSpec,
-  swaggerUiOptions,
-} = require('./configs/swagger');
+// const {
+//   swaggerUi,
+//   swaggerSpec,
+//   swaggerUiOptions,
+// } = require('./configs/swagger');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(
-  '/swagger',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, swaggerUiOptions)
-);
+// app.use(
+//   '/swagger',
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerSpec, swaggerUiOptions)
+// );
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/interests', interestRoutes);
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Server NodeJS API',
-    documentation: '/swagger',
-    version: '1.0.0',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Server NodeJS API',
+//     documentation: '/swagger',
+//     version: '1.0.0',
+//   });
+// });
 
 app.listen(process.env.PORT, () => {
   connectDB();
